9a5efbbb3131775e0f3506531abc6884e6e4fa11
Walther Chen <walther.chen@gmail.com>
2020-05-05 23:32:49 -0400

    some debugging in example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,4 +1,4 @@
-use http::Response;
+use http::{header, Response};
 use smol::{Async, Task};
 use std::net::TcpListener;
 use piper::Arc;
@@ -16,6 +16,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             let task = Task::spawn(async move {
                 let serve = accept(&addr, stream, |req, resp_wtr| async {
+                    println!("{:?}", *req.uri());
+                    println!("{:?}", req.version());
+                    println!("{:?}", req.method());
+                    println!("{:?}", req.headers().get(header::CONTENT_LENGTH));
+                    println!("{:?}", req.headers().get(header::HOST));
+
                     let req_body = req.into_body().into_string().await.unwrap();
                     let resp_body = format!("Hello, {}!", req_body);
                     let resp = Response::new(resp_body.into());
