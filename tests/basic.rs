6512dad9b1a25717dd43628fa86020a1e044988a
Walther Chen <walther.chen@gmail.com>
2020-05-07 12:07:50 -0400

    [test] basic request passes

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,6 +1,5 @@
 mod test_client;
 
-use std::sync::{Arc, Mutex};
 use http::Response as HttpResponse;
 use http::{
     header::{
@@ -39,33 +38,35 @@ fn test_empty_body() {
     });
 }
 
-//#[test]
-//fn test_basic_request() {
-//    smol::block_on(async {
-//        let testcase = TestClient { write_buf: Arc::new(Mutex::new(vec![])) };
-//        let example = b"GET /foo/bar HTTP/1.1\r\nHost: example.org\r\nContent-Length: 6\r\n\r\ntophat".to_vec();
-//
-//        let addr = "http://example.org";
-//        accept(addr, testcase.clone(), |req, resp_wtr| async move {
-//            // some basic parsing tests
-//            assert_eq!(*req.uri(), Uri::from_static("http://example.org/foo/bar"));
-//            assert_eq!(req.version(), Version::HTTP_11);
-//            assert_eq!(req.method(), Method::GET);
-//            assert_eq!(req.headers().get(header::CONTENT_LENGTH), Some(&HeaderValue::from_bytes(b"6").unwrap()));
-//            assert_eq!(req.headers().get(header::HOST), Some(&HeaderValue::from_bytes(b"example.org").unwrap()));
-//
-//            let body = req.into_body().into_string().await.unwrap();
-//
-//            let res_body = format!("Hello {}", body);
-//
-//            let resp = HttpResponse::new(res_body.into());
-//            let done = resp_wtr.send(resp).await.unwrap();
-//
-//            Ok(done)
-//        })
-//        .await
-//        .unwrap();
-//
-//        assert_eq!(testcase.out_string(), "Hello tophat".to_owned())
-//    });
-//}
+#[test]
+fn test_basic_request() {
+    smol::block_on(async {
+        let testclient = TestClient::new(
+            "GET /foo/bar HTTP/1.1\r\nHost: example.org\r\nContent-Length: 6\r\n\r\ntophat",
+            "HTTP/1.1 200 OK\r\ncontent-length: 12\r\n\r\nHello tophat",
+        );
+
+        let addr = "http://example.org";
+        accept(addr, testclient.clone(), |req, resp_wtr| async move {
+            // some basic parsing tests
+            assert_eq!(*req.uri(), Uri::from_static("http://example.org/foo/bar"));
+            assert_eq!(req.version(), Version::HTTP_11);
+            assert_eq!(req.method(), Method::GET);
+            assert_eq!(req.headers().get(header::CONTENT_LENGTH), Some(&HeaderValue::from_bytes(b"6").unwrap()));
+            assert_eq!(req.headers().get(header::HOST), Some(&HeaderValue::from_bytes(b"example.org").unwrap()));
+
+            let body = req.into_body().into_string().await.unwrap();
+
+            let res_body = format!("Hello {}", body);
+
+            let resp = HttpResponse::new(res_body.into());
+            let done = resp_wtr.send(resp).await.unwrap();
+
+            Ok(done)
+        })
+        .await
+        .unwrap();
+
+        testclient.assert();
+    });
+}
