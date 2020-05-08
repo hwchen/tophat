7202d3d77d590d31920cac13ef300e1f5c28b9b3
Walther Chen <walther.chen@gmail.com>
2020-05-07 23:15:19 -0400

    add timeout

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,12 @@ mod encode;
 mod error;
 mod request;
 mod response;
+mod timeout;
 mod util;
 
 use futures_core::Future;
 use futures_io::{AsyncRead, AsyncWrite};
+use std::time::Duration;
 
 pub use crate::body::Body;
 use crate::decode::decode;
@@ -17,37 +19,62 @@ pub use crate::error::{Error, Result};
 pub use crate::request::Request;
 use crate::response::InnerResponse;
 pub use crate::response::{ResponseWriter, ResponseWritten};
+use crate::timeout::{timeout, TimeoutError};
 
 /// Accept a new incoming Http/1.1 connection
 ///
-/// Automatically support KeepAlive
-// All errors should be bubbled up to this fn to handle, either in logs or in responses.
+/// Automatically supports KeepAlive
 pub async fn accept<RW, F, Fut>(io: RW, endpoint: F) -> Result<()>
 where
     RW: AsyncRead + AsyncWrite + Clone + Send + Sync + Unpin + 'static,
     F: Fn(Request, ResponseWriter<RW>) -> Fut,
     Fut: Future<Output = Result<ResponseWritten>>,
 {
+    accept_with_opts(io, endpoint, ServerOpts::default()).await
+}
+
+/// Accept a new incoming Http/1.1 connection
+///
+/// Automatically supports KeepAlive
+pub async fn accept_with_opts<RW, F, Fut>(io: RW, endpoint: F, opts: ServerOpts) -> Result<()>
+where
+    RW: AsyncRead + AsyncWrite + Clone + Send + Sync + Unpin + 'static,
+    F: Fn(Request, ResponseWriter<RW>) -> Fut,
+    Fut: Future<Output = Result<ResponseWritten>>,
+{
+    // All errors should be bubbled up to this fn to handle, either in logs or in responses.
+
     loop {
         // decode to Request
         let req_fut = decode(io.clone());
 
         // Handle req failure modes, timeout, eof
-        let req = req_fut.await;
-        let req= match req {
-            Ok(r) => {
-                match r {
-                    Some(r) => r,
-                    None => break, /* EOF */
+        let req = if let Some(timeout_duration) = opts.timeout {
+            // this arm is for with timeout
+            match timeout(timeout_duration, req_fut).await {
+                Ok(Ok(Some(r))) => r,
+                Ok(Ok(None)) | Err(TimeoutError { .. }) => break, // EOF or timeout
+                Ok(Err(err)) => {
+                    // send a resp for errors from decoding, and continue on to next request
+                    if let Some(err_resp) = decode::fail_to_response_and_log(err) {
+                        let _ = err_resp.send(io.clone()).await;
+                    }
+                    continue;
                 }
-            },
-            Err(err) => {
-                // send a resp for errors from decoding, and continue on to next request
-                if let Some(err_resp) = decode::fail_to_response_and_log(err) {
-                    let _ = err_resp.send(io.clone()).await;
-                }
-                continue;
-            },
+            }
+        } else {
+            // This arm is for no timeout
+            match req_fut.await {
+                Ok(Some(r)) => r,
+                Ok(None) => break, // EOF
+                Err(err) => {
+                    // send a resp for errors from decoding, and continue on to next request
+                    if let Some(err_resp) = decode::fail_to_response_and_log(err) {
+                        let _ = err_resp.send(io.clone()).await;
+                    }
+                    continue;
+                },
+            }
         };
 
         // Encode from Response happens when `ResponseWriter::send()` is called inside endpoint.
@@ -66,4 +93,16 @@ where
     }
 
     Ok(())
+}
+
+pub struct ServerOpts {
+    pub timeout: Option<Duration>,
+}
+
+impl Default for ServerOpts {
+    fn default() -> Self {
+        Self {
+            timeout: Some(Duration::from_secs(60)),
+        }
+    }
 }
---
+++ src/timeout.rs
@@ -0,0 +1,69 @@
+// From async-std future::timeout, except that futures_timer is swapped in.
+
+use std::error::Error;
+use std::fmt;
+use std::pin::Pin;
+use std::time::Duration;
+use std::future::Future;
+
+use futures_timer::Delay;
+use pin_project_lite::pin_project;
+
+use futures_core::task::{Context, Poll};
+
+pub(crate) async fn timeout<F, T>(dur: Duration, f: F) -> Result<T, TimeoutError>
+where
+    F: Future<Output = T>,
+{
+    let f = TimeoutFuture {
+        future: f,
+        delay: Delay::new(dur),
+    };
+    f.await
+}
+
+pin_project! {
+    /// A future that times out after a duration of time.
+    pub(crate) struct TimeoutFuture<F> {
+        #[pin]
+        future: F,
+        #[pin]
+        delay: Delay,
+    }
+}
+
+impl<F> TimeoutFuture<F> {
+    #[allow(dead_code)]
+    pub(crate) fn new(future: F, dur: Duration) -> TimeoutFuture<F> {
+        TimeoutFuture { future, delay: Delay::new(dur) }
+    }
+}
+
+impl<F: Future> Future for TimeoutFuture<F> {
+    type Output = Result<F::Output, TimeoutError>;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = self.project();
+        match this.future.poll(cx) {
+            Poll::Ready(v) => Poll::Ready(Ok(v)),
+            Poll::Pending => match this.delay.poll(cx) {
+                Poll::Ready(_) => Poll::Ready(Err(TimeoutError { _private: () })),
+                Poll::Pending => Poll::Pending,
+            },
+        }
+    }
+}
+
+/// An error returned when a future times out.
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub(crate) struct TimeoutError {
+    _private: (),
+}
+
+impl Error for TimeoutError {}
+
+impl fmt::Display for TimeoutError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        "future has timed out".fmt(f)
+    }
+}
