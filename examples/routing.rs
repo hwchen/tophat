6a7709bd2ad709fa58eb47eef59c00ad0d6adbd8
Walther Chen <walther.chen@gmail.com>
2020-05-10 15:37:24 -0400

    routing example w/ function type alias works

--- examples/routing.rs
+++ examples/routing.rs
@@ -1,5 +1,7 @@
-#![allow(dead_code)]
 #![allow(type_alias_bounds)]
+
+// https://users.rust-lang.org/t/how-to-handle-a-vector-of-async-function-pointers/39804
+// https://stackoverflow.com/questions/60621816/how-to-indicate-that-the-lifetime-of-an-async-functions-return-value-is-the-sam
 
 use futures_util::io::{AsyncRead, AsyncWrite};
 use http::Response;
@@ -11,9 +13,9 @@ use path_tree::PathTree;
 use piper::Arc;
 use tophat::server::{accept, Request, ResponseWriter, ResponseWritten, Result};
 
-type Params<'a> = Vec<(&'a str, &'a str)>;
+type Params = Vec<(String, String)>;
 
-type Handler<W: AsyncRead + AsyncWrite + Clone + Send + Sync + Unpin + 'static> = fn(Request, ResponseWriter<W>) -> Pin<Box<dyn Future<Output = Result<ResponseWritten>> + Send>>;
+type Handler<W: AsyncRead + AsyncWrite + Clone + Send + Sync + Unpin + 'static> = fn(Request, ResponseWriter<W>, Params) -> Pin<Box<dyn Future<Output = Result<ResponseWritten>> + Send>>;
 
 fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     pretty_env_logger::init();
@@ -28,17 +30,21 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
 
     smol::run(async {
         loop {
-            let router = Arc::clone(&tree);
+            let router = tree.clone();
 
             let (stream, _) = listener.accept().await?;
             let stream = Arc::new(stream);
 
             let task = Task::spawn(async move {
                 let serve = accept(stream, |req, resp_wtr| async {
-
                     let path = "/".to_owned() + req.method().as_str() + req.uri().path();
                     match router.find(&path) {
-                        Some((handler, _params)) => handler(req, resp_wtr).await,
+                        Some((handler, params)) => {
+                            // this gets rid of lifetime problems; borrowing the params has issues
+                            // with spawning. It's probably fast enough.
+                            let params = params.into_iter().map(|(a,b)| (a.to_owned(), b.to_owned())).collect();
+                            handler(req, resp_wtr, params).await
+                        },
                         None => {
                             let resp = http::Response::builder()
                                 .status(http::StatusCode::NOT_FOUND)
@@ -47,13 +53,11 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
                             resp_wtr.send(resp).await
                         },
                     }
-
                 }).await;
 
                 if let Err(err) = serve {
                     eprintln!("Error: {}", err);
                 }
-
             });
 
             task.detach();
@@ -62,26 +66,26 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
 }
 
 //async fn hello_user<'a, W>(_req: Request, resp_wtr: ResponseWriter<W>, params: Params<'a>) -> Result<ResponseWritten>
-async fn hello_user<W>(_req: Request, resp_wtr: ResponseWriter<W>) -> Result<ResponseWritten>
+async fn hello_user< W>(_req: Request, resp_wtr: ResponseWriter<W>, params: Params) -> Result<ResponseWritten>
     where W: AsyncRead + AsyncWrite + Clone + Send + Sync + Unpin + 'static,
 {
-    let resp_body = format!("Hello, ");
-    //for (k, v) in params {
-    //    resp_body.push_str(&format!("{} = {}", k, v));
-    //}
+    let mut resp_body = format!("Hello, ");
+    for (k, v) in params {
+        resp_body.push_str(&format!("{} = {}", k, v));
+    }
     let resp = Response::new(resp_body.into());
 
     resp_wtr.send(resp).await
 }
 
-fn pin_hello_user<W>(req: Request, resp_wtr: ResponseWriter<W>) -> Pin<Box<dyn Future<Output = Result<ResponseWritten>>+ Send>>
+fn pin_hello_user<W>(req: Request, resp_wtr: ResponseWriter<W>, params: Params) -> Pin<Box<dyn Future<Output = Result<ResponseWritten>> + Send>>
     where W: AsyncRead + AsyncWrite + Clone + Send + Sync + Unpin + 'static,
 {
-    Box::pin(hello_user(req, resp_wtr))
+    Box::pin(hello_user(req, resp_wtr, params))
 }
 
 //async fn hello_rust<'a, W>(_req: Request, resp_wtr: ResponseWriter<W>, _params: Params<'a>) -> Result<ResponseWritten>
-async fn hello_rust<W>(_req: Request, resp_wtr: ResponseWriter<W>) -> Result<ResponseWritten>
+async fn hello_rust<W>(_req: Request, resp_wtr: ResponseWriter<W>, _params: Params) -> Result<ResponseWritten>
     where W: AsyncRead + AsyncWrite + Clone + Send + Sync + Unpin + 'static,
 {
     let resp_body = format!("Hello, rust!");
@@ -90,8 +94,8 @@ async fn hello_rust<W>(_req: Request, resp_wtr: ResponseWriter<W>) -> Result<Res
     resp_wtr.send(resp).await
 }
 
-fn pin_hello_rust<W>(req: Request, resp_wtr: ResponseWriter<W>) -> Pin<Box<dyn Future<Output = Result<ResponseWritten>>+ Send>>
+fn pin_hello_rust<W>(req: Request, resp_wtr: ResponseWriter<W>, params: Params) -> Pin<Box<dyn Future<Output = Result<ResponseWritten>> + Send>>
     where W: AsyncRead + AsyncWrite + Clone + Send + Sync + Unpin + 'static,
 {
-    Box::pin(hello_rust(req, resp_wtr))
+    Box::pin(hello_rust(req, resp_wtr, params))
 }
