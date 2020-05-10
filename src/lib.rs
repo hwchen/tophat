c7f7ac99942f10a5fdcd359b7b085dd58b098c63
Walther Chen <walther.chen@gmail.com>
2020-05-09 21:03:17 -0400

    [docs] start lib docs, add notes on constructing a Response

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,20 @@
 #![deny(unsafe_code)]
 
+//! # tophat
+//!
+//! ## Constructing a Response:
+//! - a `tophat::Response` is just an alias for an `http::Response<Body>`, where `Body` is
+//! tophat-specific streaming body. There are convenience methods for constructing bodies easily
+//! from streams and buffers.
+//! - content-type headers must be set manually. tophat exposes the the `mime` lib for those who
+//! wish to use types for MIME.
+//! - body-type headers are ignored, because the encoder sets them depending on the type of body
+//! set:
+//!   - from streaming reader with length: fixed body
+//!   - from streaming reader without length: transfer-encoding, chunked
+//!   - from buffer (`Vec<u8>` or `String` or `&str`): fixed body
+//! - In the future, there may be some convenience methods for constructing common responses.
+
 mod body;
 mod chunked;
 mod decode;
