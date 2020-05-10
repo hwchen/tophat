cce1402d6ed7209e6346ee48957b4adb9ac9726c
Walther Chen <walther.chen@gmail.com>
2020-05-10 17:23:23 -0400

    move routing examples around

--- examples/routing_1.rs
+++ examples/routing_1.rs
@@ -1,3 +1,8 @@
+#![allow(type_alias_bounds)]
+
+// https://users.rust-lang.org/t/how-to-handle-a-vector-of-async-function-pointers/39804
+// https://stackoverflow.com/questions/60621816/how-to-indicate-that-the-lifetime-of-an-async-functions-return-value-is-the-sam
+
 use futures_util::io::{AsyncRead, AsyncWrite};
 use http::Response;
 use smol::{Async, Task};
@@ -10,32 +15,36 @@ use tophat::server::{accept, Request, ResponseWriter, ResponseWritten, Result};
 
 type Params = Vec<(String, String)>;
 
+type Handler<W: AsyncRead + AsyncWrite + Clone + Send + Sync + Unpin + 'static> =
+    fn(Request, ResponseWriter<W>, Params) -> Pin<Box<dyn Future<Output = Result<ResponseWritten>> + Send>>;
+
 fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     pretty_env_logger::init();
 
     //let mut tree = PathTree::<Box<dyn Endpoint<_>>>::new();
-    let mut tree = PathTree::<Box<dyn Endpoint<_>>>::new();
-    tree.insert("/GET/:name", Box::new(hello_user));
-    tree.insert("/GET/", Box::new(hello_rust));
+    let mut tree = PathTree::<Handler<_>>::new();
+    tree.insert("/GET/:name", pin_hello_user);
+    tree.insert("/GET/rust", pin_hello_rust);
     let tree = Arc::new(tree);
 
     let listener = Async::<TcpListener>::bind("127.0.0.1:9999")?;
 
     smol::run(async {
         loop {
-            let router = Arc::clone(&tree);
+            let router = tree.clone();
 
             let (stream, _) = listener.accept().await?;
             let stream = Arc::new(stream);
 
             let task = Task::spawn(async move {
                 let serve = accept(stream, |req, resp_wtr| async {
-
                     let path = "/".to_owned() + req.method().as_str() + req.uri().path();
                     match router.find(&path) {
                         Some((handler, params)) => {
+                            // this gets rid of lifetime problems; borrowing the params has issues
+                            // with spawning. It's probably fast enough.
                             let params = params.into_iter().map(|(a,b)| (a.to_owned(), b.to_owned())).collect();
-                            handler.call(req, resp_wtr, params).await
+                            handler(req, resp_wtr, params).await
                         },
                         None => {
                             let resp = http::Response::builder()
@@ -45,13 +54,11 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
                             resp_wtr.send(resp).await
                         },
                     }
-
                 }).await;
 
                 if let Err(err) = serve {
                     eprintln!("Error: {}", err);
                 }
-
             });
 
             task.detach();
@@ -59,7 +66,8 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     })
 }
 
-async fn hello_user<W>(_req: Request, resp_wtr: ResponseWriter<W>, params: Params) -> Result<ResponseWritten>
+//async fn hello_user<'a, W>(_req: Request, resp_wtr: ResponseWriter<W>, params: Params<'a>) -> Result<ResponseWritten>
+async fn hello_user< W>(_req: Request, resp_wtr: ResponseWriter<W>, params: Params) -> Result<ResponseWritten>
     where W: AsyncRead + AsyncWrite + Clone + Send + Sync + Unpin + 'static,
 {
     let mut resp_body = format!("Hello, ");
@@ -71,37 +79,24 @@ async fn hello_user<W>(_req: Request, resp_wtr: ResponseWriter<W>, params: Param
     resp_wtr.send(resp).await
 }
 
+fn pin_hello_user<W>(req: Request, resp_wtr: ResponseWriter<W>, params: Params) -> Pin<Box<dyn Future<Output = Result<ResponseWritten>> + Send>>
+    where W: AsyncRead + AsyncWrite + Clone + Send + Sync + Unpin + 'static,
+{
+    Box::pin(hello_user(req, resp_wtr, params))
+}
+
+//async fn hello_rust<'a, W>(_req: Request, resp_wtr: ResponseWriter<W>, _params: Params<'a>) -> Result<ResponseWritten>
 async fn hello_rust<W>(_req: Request, resp_wtr: ResponseWriter<W>, _params: Params) -> Result<ResponseWritten>
     where W: AsyncRead + AsyncWrite + Clone + Send + Sync + Unpin + 'static,
 {
-    //let resp_body = format!("Hello, rust!");
-    //let resp = Response::new(resp_body.into());
-    let resp = Response::new(tophat::Body::empty());
+    let resp_body = format!("Hello, rust!");
+    let resp = Response::new(resp_body.into());
 
     resp_wtr.send(resp).await
 }
 
-pub(crate) type BoxFuture<'a, T> = Pin<Box<dyn Future<Output = T> + Send + 'a>>;
-
-pub trait Endpoint<W>: Send + Sync + 'static
+fn pin_hello_rust<W>(req: Request, resp_wtr: ResponseWriter<W>, params: Params) -> Pin<Box<dyn Future<Output = Result<ResponseWritten>> + Send>>
     where W: AsyncRead + AsyncWrite + Clone + Send + Sync + Unpin + 'static,
 {
-    /// Invoke the endpoint within the given context
-    fn call<'a>(&'a self, req: Request, resp_wtr: ResponseWriter<W>, params: Params) -> BoxFuture<'a, Result<ResponseWritten>>;
-}
-
-impl<F: Send + Sync + 'static, Fut, Res, W> Endpoint<W> for F
-where
-    F: Fn(Request, ResponseWriter<W>, Params) -> Fut,
-    Fut: Future<Output = Result<Res>> + Send + 'static,
-    Res: Into<ResponseWritten>,
-    W: AsyncRead + AsyncWrite + Clone + Send + Sync + Unpin + 'static,
-{
-    fn call<'a>(&'a self, req: Request, resp: ResponseWriter<W>, params: Params) -> BoxFuture<'a, Result<ResponseWritten>> {
-        let fut = (self)(req, resp, params);
-        Box::pin(async move {
-            let res = fut.await?;
-            Ok(res.into())
-        })
-    }
+    Box::pin(hello_rust(req, resp_wtr, params))
 }
